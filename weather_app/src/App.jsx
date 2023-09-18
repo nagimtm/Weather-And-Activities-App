8bc5d465998a8fa98fbc9790a6095592a98879c7
nagim <a.nagim@protonmail.com>
2023-09-18 15:15:28 +0200

    add weatherFetch funciton

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -1,4 +1,14 @@
 function App() {
+  try {
+    async function weatherFetch() {
+      const response = await fetch(
+        "https://example-apis.vercel.app/api/weather"
+      );
+      const data = await response.json();
+      console.log("data", data);
+    }
+  } catch (error) {}
+  weatherFetch();
   return;
 }
 
