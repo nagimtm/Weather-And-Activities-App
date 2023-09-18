92a5a2830cfbb93e85f1a9c190d4d6a271c0e8c3
nagim <a.nagim@protonmail.com>
2023-09-18 17:46:02 +0200

    Add fetch function and fetch the data from api

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -1,15 +1,21 @@
+import { useState, useEffect } from "react";
 function App() {
-  try {
-    async function weatherFetch() {
+  const [] = useState();
+  async function weatherFetch() {
+    try {
       const response = await fetch(
         "https://example-apis.vercel.app/api/weather"
       );
       const data = await response.json();
       console.log("data", data);
+    } catch (error) {
+      console.log("Error", error);
     }
-  } catch (error) {}
-  weatherFetch();
-  return;
+  }
+  useEffect(() => {
+    weatherFetch();
+  });
+  //return
 }
 
 export default App;
